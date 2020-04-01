93e47705b4e764caff3c951027feb2709197c4e7
Tom <tveeder@live.com>
2020-03-31 23:28:17 -0500

    interactive map andppt slides added

--- logic.js
+++ logic.js
@@ -1,18 +1,169 @@
-// Creating our initial map object
-// We set the longitude, latitude, and the starting zoom level
-// This gets inserted into the div with an id of 'map'
-var myMap = L.map("map", {
-    center: [41.881832, -87.623177],
-    zoom: 13
+// Function to determine marker size based on initialsize
+function markerSize(initialsize) {
+    return initialsize / 4000;
+}
 
-});
+// An array containing all of the information needed to create city and beach markers
+var locations = [{
+        coordinates: [41.8935, -87.6129],
+        beach: {
+            name: "Ohio",
+            initialsize: 197950,
+            turbidity: 888,
+            temperature: 58585,
+            waves: 88588,
+            E_coli: 9999,
+        },
+    },
+
+    {
+        coordinates: [41.9663, -87.6372],
+        beach: {
+            name: "Montrose",
+            initialsize: 392500,
+            turbidity: 95959,
+            temperature: 884848,
+            waves: 88888,
+            E_coli: 84848
+
+        },
+
+    },
+
+    {
+        coordinates: [41.7144, -87.5283],
+        beach: {
+            name: "Calumet Beach",
+            initialsize: 226718,
+            turbidity: 4747373,
+            temperature: 85858,
+            waves: 85858,
+            E_coli: 4474747
+        },
+
+    },
+
+    {
+        coordinates: [41.986186, -87.651997],
+        beach: {
+            name: "Osterman",
+            initialsize: 269600,
+            turbidity: 244747,
+            temperature: 84848,
+            waves: 884848,
+            E_coli: 33333
+        },
+
+    },
+
+    {
+        coordinates: [41.83946, -87.60622],
+        beach: {
+            name: "Margaret Burroughs",
+            initialsize: 388200,
+            turbidity: 4774747,
+            temperature: 888800,
+            waves: 22388,
+            E_coli: 11181
+        },
+
+    }
+];
+
+// Define arrays to hold created city and beach markers
+var turbidityMarkers = [];
+var waveMarkers = [];
+var tempMarkers = [];
+var E_coliMarkers = [];
 
 
-// Adding a tile layer (the background map image) to our map
-// We use the addTo method to add objects to our map
-L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+for (var i = 0; i < locations.length; i++) {
+    // Setting the marker radius for the beach by passing initialsize into the markerSize function
+    turbidityMarkers.push(
+        L.circle(locations[i].coordinates, {
+            stroke: false,
+            fillOpacity: 0.75,
+            color: "grey",
+            fillColor: "grey",
+            radius: markerSize(locations[i].beach.turbidity)
+        })
+    );
+    tempMarkers.push(
+        L.circle(locations[i].coordinates, {
+            stroke: false,
+            fillOpacity: 0.75,
+            color: "red",
+            fillColor: "red",
+            radius: markerSize(locations[i].beach.temperature)
+        })
+    );
+    E_coliMarkers.push(
+        L.circle(locations[i].coordinates, {
+            stroke: false,
+            fillOpacity: 0.75,
+            color: "black",
+            fillColor: "black",
+            radius: markerSize(locations[i].beach.E_coli)
+        })
+    );
+
+
+    // Setting the marker radius for the city by passing initialsize into the markerSize function
+    waveMarkers.push(
+        L.circle(locations[i].coordinates, {
+            stroke: false,
+            fillOpacity: 0.75,
+            color: "blue",
+            fillColor: "blue",
+            radius: markerSize(locations[i].beach.initialsize)
+        })
+    );
+}
+
+// Define variables for our base layers
+var streetmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
     id: "mapbox.streets",
-    accessToken: API_KEY,
+    accessToken: API_KEY
+});
+
+var darkmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.dark",
+    accessToken: API_KEY
+});
+
+// Create two separate layer groups: one for waves and one for Turbidity
+var turbidity = L.layerGroup(turbidityMarkers);
+var waves = L.layerGroup(waveMarkers);
+var temperature = L.layerGroup(tempMarkers);
+var E_coli = L.layerGroup(E_coliMarkers);
+
+// Create a baseMaps object
+var baseMaps = {
+    "Street Map": streetmap,
+    "Dark Map": darkmap
+};
+
+// Create an overlay object
+var overlayMaps = {
+    "Turbidity": turbidity,
+    "Wave Height": waves,
+    "Temperature": temperature,
+    "E_coli": E_coli
+};
+
+// Define a map object
+var myMap = L.map("map", {
+    center: [41.8781, -87.6298],
+    zoom: 15,
+    layers: [streetmap, turbidity, waves, temperature, E_coli]
+});
+
+// Pass our map layers into our layer control
+// Add the layer control to the map
+L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
 }).addTo(myMap);
